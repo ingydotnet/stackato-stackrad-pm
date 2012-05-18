b2a019001bba6d5cff2e94572911094d868b7fde
Ingy döt Net <ingy@ingy.net>
2012-05-18 19:23:11 +0000

    Get login token.

--- lib/Stackato/Stackrad.pm
+++ lib/Stackato/Stackrad.pm
@@ -11,6 +11,7 @@ use Curses::UI 0;
 use LWP::UserAgent 0;
 use LWP::Protocol::https 0;
 use HTTP::Request 0;
+use URI::Escape 0;
 use JSON::XS 0;
 use YAML::XS 0;
 # use XXX;
@@ -32,13 +33,15 @@ use constant target_key_hint => ' (set target with Ctrl+t)';
 use constant default_title => app_name . target_key_hint;
 use constant new_target_prompt =>
     "New target? (e.g., api.stackato.example.com)";
+use constant username_prompt => "Username:";
+use constant password_prompt => "Password:";
 use constant user_agent_string =>
     app_name . "/$VERSION lwp/$LWP::UserAgent::VERSION";
 use constant main_color => 'cyan';
 use constant secondary_color => 'cyan';
 use constant accent_color => 'red';
 
-has targets => (default => sub{[]});
+has targets => (default => sub{[{url=>'api.stacka.to'}]});
 
 has target_index => ();
 has cui => ();
@@ -124,6 +127,7 @@ sub run {
 
 sub setup_cui {
     my $self = shift;
+    $self->target_index($#{$self->targets}) if @{$self->targets};    # XXX
     my $cui = $self->{cui} = $self->cui(
         Curses::UI->new(
             -color_support => 1,
@@ -134,7 +138,7 @@ sub setup_cui {
     $cui->set_binding(sub { exit 0 }, "\cC");
     $cui->set_binding(sub { $self->prompt_for_target }, "\cT");
     $cui->set_binding(sub { $self->delete_current_target }, "\cX");
-    $cui->set_binding(sub { PPP @_ }, "\c1");
+    $cui->set_binding(sub { $self->login }, "\cL");
 
     my $win1 = $self->{win1} =
         $cui->add('win1', 'Window',
@@ -183,12 +187,12 @@ sub current_target {
 
 sub prompt_for_target {
     my $self = shift;
-    my $answer = $self->cui->question(new_target_prompt);
+    my $answer = $self->cui->question(new_target_prompt); # TODO: "api."
     return unless $answer;
 
     return $self->error($answer . " does not appear to be a valid Stackato VM")
         unless $self->validate_target($answer);
-    push @{$self->targets}, $answer;
+    push @{$self->targets}, {url => $answer};
     $self->target_index($#{$self->targets});
     $self->update_targets_screen;
     $self->set_title
@@ -211,16 +215,46 @@ sub update_targets_screen {
     my $tab = $self->tab_named('Targets');
     my $out = '';
     for (0 .. $#{$self->targets}) {
+        my $target = $self->targets->[$_];
         $out .= $_ == $self->target_index ? ' * ' : '   ';
-        $out .= $self->targets->[$_] . "\n";
+        $out .= $target->{url};
+        $out .= $target->{user}
+            ? " (${\$target->{user}}) "
+            : " (not logged in) ";
+        $out .= "\n";
     }
-    $out .= "\nPress 'Ctrl+t' to add a target.";
-    $out .= "\n\nPress 'Ctrl+x' to delete current target."
-        if @{$self->targets};
+    $out .= "\n\n";
+    if (@{$self->targets}) {
+        $out .= "Ctrl+L to login.\n"
+            unless $self->logged_in;
+        $out .= "Ctrl+x to delete current target.\n"
+    }
+    $out .= "Ctrl+t to add a target.\n";
 #     $out .= "\n\nPress 'Ctrl+<target #>' to set current target."
 #         if @{$self->targets} > 1;
     $tab->{tv}{-text} = $out;
     $self->win1->draw(1);
+}
+
+sub login {
+    my $self = shift;
+    my $username = $self->cui->question(username_prompt); # TODO: <prev-user>
+    return unless $username;
+    my $password = $self->cui->question(password_prompt); # TODO: <prev-user>
+    return unless $password;
+    my $path = '/users/' . uri_escape($username) . '/tokens';
+    $password = quotemeta($password);
+    my $response = $self->post($path, qq({"password":"$password"}));
+    return $self->error("Couldn't login.") unless $response->is_success; 
+    my $token = decode_json($response->content)->{token};
+    $self->current_target->{user} = $username;
+    $self->current_target->{token} = $token;
+    $self->update_targets_screen;
+}
+
+sub logged_in {
+    my $self = shift;
+    $self->current_target->{user}
 }
 
 sub set_title {
@@ -234,6 +268,27 @@ sub validate_target {
     my $response = $self->get_from_target($target, '/info/');
     return unless $response->is_success; 
     decode_json($response->content)
+}
+
+sub post {
+    my ($self, $path, $data) = @_;
+    # XXX "hostname", not "url"
+    $self->post_to_target($self->current_target->{url}, $path, $data)
+}
+sub post_to_target {
+    my ($self, $target, $path, $data) = @_;
+    my $ua = LWP::UserAgent->new(agent => user_agent_string);
+    warn "Stackrad being lazy and disabling SSL cert verification!";
+    $ua->ssl_opts(
+        verify_hostname => 0,
+        #? SSL_ca_path => '/app/fs/pair/certcert/stackato.ddns.us.pem',
+    );
+    my $request = HTTP::Request->new('POST', 'https://'.$target.$path);
+    $request->header('Accept' => 'application/json');
+    $request->content($data);
+    #? cookies?
+    #? $request->header( 'Content-Type' => $p{type} )     if $p{type};
+    $ua->simple_request($request)
 }
 
 sub get_from_target {
